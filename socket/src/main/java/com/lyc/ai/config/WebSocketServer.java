a05facae7f96a1529ac468448c1e2515e0fc001a
lycGit <1148589531@qq.com>
2025-06-28 10:49:08 +0800

    可以区分用户的socket服务本地联调成功

--- socket/src/main/java/com/lyc/ai/config/WebSocketServer.java
+++ socket/src/main/java/com/lyc/ai/config/WebSocketServer.java
@@ -1,38 +1,89 @@
 package com.lyc.ai.config;
 
 import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArraySet;
 
 import javax.websocket.OnClose;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
+import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 
+import com.github.pagehelper.util.StringUtil;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.stereotype.Component;
 
-@ServerEndpoint(value="/webSocket")
+import java.util.HashMap;
+import java.util.Map;
+
+@ServerEndpoint(value="/webSocket/{userId}")
 @Component
 public class WebSocketServer {
-
+    // 新增静态缓存集合，用于存储在线用户
+    private static ConcurrentHashMap<String, WebSocketServer> onlineUsers = new ConcurrentHashMap<>();
     private Session session;
-    public static CopyOnWriteArraySet<WebSocketServer> webSockets = new CopyOnWriteArraySet<WebSocketServer>();
+    private String userId = "";
 
     @OnOpen
-    public void onOpen(Session session) throws InterruptedException {
+    public void onOpen(Session session, @PathParam("userId") String userId) throws InterruptedException {
         this.session = session;
-        webSockets.add(this);
-        this.send("新用户加入");
+        this.userId = userId;
+        if (onlineUsers.containsKey(userId)) {
+            onlineUsers.remove(userId);
+            onlineUsers.put(userId, this);
+        } else {
+            onlineUsers.put(userId, this);
+        }
+        try {
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("msg", "连接成功！");
+            jsonObject.put("userId", userId);
+            this.sendMessage(jsonObject);
+        } catch (Exception e){
+
+        }
     }
+
     @OnClose
     public void onClose(Session s) {
-        webSockets.remove(this);
-        this.send("有用户离开");
+        // 从缓存中移除离开的用户
+        if (onlineUsers.containsKey(userId)) {
+            onlineUsers.remove(userId);
+            try {
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.put("msg", "用户离开");
+                jsonObject.put("userId", userId);
+                this.sendMessage(jsonObject);
+            } catch (Exception e){
+
+            }
+        }
     }
     @OnMessage
-    public void onMessage(String msg) throws InterruptedException {
+    public void onMessage(String msg, Session session) throws InterruptedException {
         System.out.println("从客服端接受的消息： "+msg);
-        this.send("从客服端接受的消息： "+msg);
+        if (StringUtil.isNotEmpty(msg)) {
+            try {
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.put("msg", msg);
+                jsonObject.put("userId", userId);
+                this.sendMessage(jsonObject);
+            } catch (Exception e){
+
+            }
+        }
+    }
+
+    public void sendMessage(JSONObject jsonObject) {
+        try {
+            if (StringUtil.isNotEmpty(userId) &&  onlineUsers.containsKey(userId)) {
+                onlineUsers.get(userId).send(jsonObject.toString());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void send(String msg) {
